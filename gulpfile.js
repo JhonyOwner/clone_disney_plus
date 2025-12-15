36cfe9f5a0f4362d712765aa5ac12653d5c28bf1
JhonyOwner <joao.vgbrisola@icloud.com>
2025-12-15 19:57:32 -0300

    Minificado as imagens

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ function styles() {
 }
 
 function images() {
-    return gulp.src('src/images/**/*')
+    return gulp.src('./src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
